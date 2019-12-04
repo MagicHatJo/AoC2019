e42d3369d1966566fd2f62091b4ead61644f56c8
MagicHatJo <42.jochang@gmail.com>
2019-12-04 14:47:36 -0800

    cleaning up 04

--- 04/01.py
+++ 04/01.py
@@ -3,27 +3,17 @@
 MIN = 152085
 MAX = 670283
 
-def valid_number(n):
+def is_valid(n):
     current = 0
+    m = [0] * 10
     for i in str(n):
         if int(i) < current:
             return False
         current = int(i)
+        m[int(i)] += 1
+    if all(x < 2 for x in m):
+        return False
     return True
 
-def has_double(n):
-    prev = 10
-    for i in str(n):
-        if prev == int(i):
-            return True
-        prev = int(i)
-    return False
-
 if __name__ == '__main__':
-    count = 0
-    i = MIN
-    while i <= MAX:
-        if valid_number(i) and has_double(i):
-            count += 1
-        i += 1
-    print(count)
+    print(len([x for x in range(MIN, MAX) if is_valid(x)]))
